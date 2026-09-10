ab996d7d14fc22824b97b93bb418a714b63f9b7d
Daniel <danielviasus99@gmail.com>
2026-09-09 19:33:56 -0500

    Prueba 2

--- Gestion_clientes/urls.py
+++ Gestion_clientes/urls.py
@@ -4,5 +4,5 @@ from .views import home_nuevo_registro, home_principal, home_registros
 urlpatterns = [
     path('', home_nuevo_registro, name='home_nuevo_registro'),
     path('home/', home_principal, name='home'),
-    path('registros/', home_registros, name='home_registros')
+    path('clientes/', home_registros, name='home_registros')
 ]
